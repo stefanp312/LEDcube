de25e9d711d6492bc38d6c1b88dee2db86a7b4fc
IrynaShv <iryna.shvydchenko@gmail.com>
2015-07-21 15:37:42 -0400

    Almost finished.

--- LEDcube.ino
+++ LEDcube.ino
@@ -1,4 +1,3 @@
-
 //THE CONSTANT SIZE OF THE CUBE
 const int CUBE_SIZE = 4;
 
@@ -6,9 +5,11 @@ const int CUBE_SIZE = 4;
 //The 3D array that is mapped to the cube
 bool dataSource[CUBE_SIZE][CUBE_SIZE][CUBE_SIZE];
 
-//Stupid variable that counts
-int counter;
+bool writeValue;
 
+//Stupid variable that counts
+volatile int counter;
+volatile int count;
 //millis timer
 volatile long unsigned int millis_timer;
 
@@ -20,10 +21,10 @@ const int clockPin = 12;
 const int dataPin = 11;
 
 //ground for layer one and ground for layer 2
-const int ground1 = 2;
-const int ground2 = 3;
-const int ground3 = 4;
-const int ground4 = 5;
+const int ground1 = 3;
+const int ground2 = 4;
+const int ground3 = 5;
+const int ground4 = 6;
 
 //holders for infromation you're going to pass to shifting function
 byte data1 = 0;
@@ -36,25 +37,27 @@ int layerAddress[CUBE_SIZE];
 unsigned int timerStuff=0;
 
 void setup() {
-  
+
   //Serial monitor for debug
   Serial.begin(9600);
 
   millis_timer = 0;
-  
+
+  count = 0;
+  randomSeed(analogRead(0));
   noInterrupts();           // disable all interrupts
   TCCR2B = 0;
   TCCR2A = 0;
-  
+
   //Set up the timer being used to control the patterns on cube
   TCCR2B |= (_BV(CS22)); //Set Timer2 prescaling factor to 64
   TCCR2B &= ~(_BV(CS20) | _BV(CS21));
   TIMSK2 |= (_BV(TOIE2));   // enable timer2 overflow interrupt
   interrupts();             // enable all interrupts
-     
-  //makes the array we are using to address the bits.
+
+    //makes the array we are using to address the bits.
   makeData();
-   
+
   //The latch pin used to update the shift registers
   pinMode(latchPin, OUTPUT);
   //The clock pin pulses when new data is fed to the shift register
@@ -67,20 +70,27 @@ void setup() {
   pinMode(ground2, OUTPUT);
   pinMode(ground3, OUTPUT);
   pinMode(ground4, OUTPUT);
-  
+  pinMode(13, OUTPUT);
+  digitalWrite(13,HIGH);
+  attachInterrupt(0, event_Handler, RISING);
   //This initializes the data source to zeros
   writeValueToEntireDataSource(0);
   //This updates the data source
   updateCubeWithDataSource();
-  
+
   //initiliazes the counter variable for seeding things temporaly
   counter = 0;
+  
+  //value for a pattern
+  writeValue = 1;
 }
 
 void loop() {
   //update the function being dispayed by the cube
   //update the cube to be synced to the datasource
-  applyPattern(0);
+  //Serial.println(count);
+  applyPattern(count);
+
   updateCubeWithDataSource();
 }
 
@@ -108,17 +118,59 @@ void printDataLayer(int layer) {
 
 //This will be main pattern selection method
 void applyPattern(int pattern) {
+
   switch (pattern) {
-    case 0:
-      powerUp();
-      break;
-    case 1:
-      testPattern();
-      break;
-    default:
-      break;
+  case 0:
+    powerUp();
+    break;
+  case 1:
+    testPattern();
+    break;
+  case 2:
+    rotatingRectangles();
+    break;
+  case 3:
+    lineSweep();
+    break;
+  case 4:
+    lightCubeFace();
+    break;
+  case 5:
+    rotatingRectanglesTakeTwo();
+    break;
+  case 6:
+    RandomLights();
+    break;
+  case 7:
+    BlinkyRand();
+    break;  
+  case 8:
+    RandomLightsGrowAndShrink();
+    break;
+  case 9:
+
+    break;
+  case 10:
+    testFunction();
+    break;
+  default:
+    count = 0;
+    break;
   }
 }
+
+//This function is responsible for changing the state of the current animation that is being displayed on the screen
+//It does this by determining if a set amount of time has occured and incrementing a global counter variable
+void stateChange(int timeInMilliSeconds){
+
+  if (Millis() >= timeInMilliSeconds){
+    //increment the state variable
+    counter++;
+    millis_timer = 0;
+
+  }
+}
+
 
 //This makes a square from the two ranges passed in on the layer provided with the value passed in
 void makeSquareOnDataSourceLayer(int x1, int x2, int y1, int y2, int z, int value){
@@ -132,8 +184,10 @@ void makeSquareOnDataSourceLayer(int x1, int x2, int y1, int y2, int z, int valu
 void makeCubeOnDataSourceLayer(int x1, int x2, int y1, int y2, int z1, int z2, int value){
   for (int i = x1; i < x2; i++) {
     for (int j = y1; j < y2; j++) {
-      for (int z = z1; z< z2; z++)
+      for (int z = z1; z< z2; z++){
         dataSource[i][j][z] = value;
+      }
+    }
   }
 }
 
@@ -141,25 +195,151 @@ void functionALL() {
   writeValueToEntireDataSource(1);
 }
 
-//This function is responsible for changing the state of the current animation that is being displayed on the screen
-//It does this by determining if a set amount of time has occured and incrementing a global counter variable
-void stateChange(int timeInMilliSeconds){
-  
-  if (Millis() >= timeInMilliSeconds){ 
-    //increment the state variable
-
+void RandomLights() {
+  // 1 = z + x + y 
+  // basically we need two independant variables so I shall use time and a value that I increment
+  stateChange(100);
+  if(counter==0){
+    writeValueToEntireDataSource(0);
+    int x = random(4);
+    int y = random(4);
+    int z = random(4);
+    Serial.println( x );
+    dataSource[x][y][z]=1;
     counter++;
-    millis_timer = 0;
-    
+  }
+  if (counter==1){
+  }
+  if (counter==2){
+    counter=0;
+  }
+}
+void RandomLightsGrow() {
+  // 1 = z + x + y 
+  // basically we need two independant variables so I shall use time and a value that I increment
+  stateChange(100);
+  if(counter==0){
+
+    int x = 0;
+    int y = 0;
+    int z = 0;
+    do{
+      x = random(4);
+      y = random(4);
+      z = random(4);
+    }
+    while(dataSource[x][y][z]==1);
+
+    dataSource[x][y][z]=1;
+    counter++;
+  }
+  if (counter==1){
+  }
+  if (counter==2){
+    if (checkValueInEntireDataSource(0)==1){
+      writeValueToEntireDataSource(0);
+    }
+    counter=0;
+  }
+}
+
+void RandomLightsGrowAndShrink() {
+  // 1 = z + x + y 
+  // basically we need two independant variables so I shall use time and a value that I increment
+  stateChange(100);
+  
+  if(counter==0){
+
+    int x = 0;
+    int y = 0;
+    int z = 0;
+    do{
+      x = random(4);
+      y = random(4);
+      z = random(4);
+    }
+   while(dataSource[x][y][z]==writeValue);
+
+    dataSource[x][y][z]= writeValue;
+    counter++;
+  }
+  if (counter==1){
+  }
+  if (counter==2){
+    if (checkValueInEntireDataSource(0)==1 ){
+      writeValue = 0;
+    }else if (checkValueInEntireDataSource(1)==1 ){
+      writeValue = 1;
+    }
+
+    counter=0;
+  }
+}
+
+//This goes through every value in the matrix and checks for a value. Returns true if all lit or not
+bool checkValueInEntireDataSource(bool value) {
+  for (int i = 0; i < CUBE_SIZE; i++) {
+    for (int j = 0; j < CUBE_SIZE; j++) {
+      for (int k = 0; k < CUBE_SIZE; k++) {
+        if(dataSource[i][j][k] == value){
+          return 0;
+        }
+      }
+    }
+  }
+  return 1;
+}
+
+void testFunction(){
+  stateChange(250);
+  if (counter == 0)
+  {
+    writeValueToEntireDataSource(0);
+    int x = 0;
+    int y = 0;
+    int z = 0;
+      x = random(4);
+      y = random(4);
+      z = (x+y)%CUBE_SIZE;
+    for (int i = 0; i <= x; i++) {
+      for (int j = 0; j <= y; j++) {
+        for (int k = 0; k <= z; k++)
+          dataSource[i][j][k] = 1;
+      }
+    }
+    counter++;
+  }
+  if (counter == 1){
+  }
+  if (counter == 2){
+    counter = 0;
+  }
+}
+
+
+void BlinkyRand() {
+  // 1 = z + x + y 
+  // basically we need two independant variables so I shall use time and a value that I increment
+  stateChange(350);
+  if(counter==0){
+    writeValueToEntireDataSource(0);
+    int x = random(4);
+    int y = random(4);
+    int z = random(4);
+    dataSource[x][y][z]=1;
+  }
+  if (counter==1){
+  }
+  if (counter==2){
+    counter=0;
   }
 }
 
 //Unoptimized pattern
 void powerUp() {
   //This changes the state every 300 milliseconds AKA inc counter every 300 milliseconds
+  stateChange(200);
 
-  stateChange(500);
- 
   if (counter==0){
     writeValueToEntireDataSource(0);
     makeSquareOnDataSourceLayer(0,CUBE_SIZE,0,CUBE_SIZE,0,1);
@@ -187,17 +367,21 @@ void powerUp() {
   if (counter==6){
     writeValueToEntireDataSource(0);
     makeSquareOnDataSourceLayer(0,CUBE_SIZE,0,CUBE_SIZE,0,1);
-    
+  }   
+  if (counter==7){
+    writeValueToEntireDataSource(0);
+    makeSquareOnDataSourceLayer(0,CUBE_SIZE,0,CUBE_SIZE,0,1);
+
     //In your last state reset that state counter
     counter = 0;
-  }  
+  }
 }
 
 void rotatingRectangles() {
   //This changes the state every 300 milliseconds AKA inc counter every 300 milliseconds
 
-  stateChange(500);
- 
+    stateChange(100);
+
   if (counter==0){
     writeValueToEntireDataSource(0);
     makeCubeOnDataSourceLayer(0,2,0,2,0,CUBE_SIZE,1);
@@ -208,7 +392,7 @@ void rotatingRectangles() {
   }
   if (counter==2){
     writeValueToEntireDataSource(0);
-   makeCubeOnDataSourceLayer(2,4,0,2,0,CUBE_SIZE,1);
+    makeCubeOnDataSourceLayer(2,4,0,2,0,CUBE_SIZE,1);
   }
   if (counter==3){
     writeValueToEntireDataSource(0);
@@ -225,26 +409,170 @@ void rotatingRectangles() {
   if (counter==6){
     writeValueToEntireDataSource(0);
     makeCubeOnDataSourceLayer(0,2,2,4,0,CUBE_SIZE,1);
-    
+  }
+
   if (counter==7){
     writeValueToEntireDataSource(0);
     makeCubeOnDataSourceLayer(0,2,1,3,0,CUBE_SIZE,1);
-    
+  }
+  if (counter==8){
+    writeValueToEntireDataSource(0);
+    makeCubeOnDataSourceLayer(0,2,1,3,0,CUBE_SIZE,1);
+
     //In your last state reset that state counter
     counter = 0;
+
   }  
+}
+
+void rotatingRectanglesTakeTwo() {
+  //This changes the state every 300 milliseconds AKA inc counter every 300 milliseconds
+
+  stateChange(170);
+
+  if (counter==0){
+    writeValueToEntireDataSource(0);
+    makeCubeOnDataSourceLayer(0,2,0,2,0,CUBE_SIZE,1);
+    makeCubeOnDataSourceLayer(2,4,2,4,0,CUBE_SIZE,1);
+  }
+  if (counter==1){
+    writeValueToEntireDataSource(0);
+    makeCubeOnDataSourceLayer(1,3,0,2,0,CUBE_SIZE,1);
+    makeCubeOnDataSourceLayer(1,3,2,4,0,CUBE_SIZE,1);
+  }
+  if (counter==2){
+    writeValueToEntireDataSource(0);
+    makeCubeOnDataSourceLayer(2,4,0,2,0,CUBE_SIZE,1);
+    makeCubeOnDataSourceLayer(0,2,2,4,0,CUBE_SIZE,1);
+  }
+  if (counter==3){
+    writeValueToEntireDataSource(0);
+    makeCubeOnDataSourceLayer(2,4,1,3,0,CUBE_SIZE,1);
+    makeCubeOnDataSourceLayer(0,2,1,3,0,CUBE_SIZE,1);
+
+  }
+  if (counter==4){
+    writeValueToEntireDataSource(0);
+    makeCubeOnDataSourceLayer(2,4,1,3,0,CUBE_SIZE,1);
+    makeCubeOnDataSourceLayer(0,2,1,3,0,CUBE_SIZE,1);
+    counter =0 ;
+  }
+}
+void Xpat ( int z, int val)
+{
+  dataSource[0][z][0]=val;
+  dataSource[1][z][1]=val;
+  dataSource[2][z][2]=val;
+  dataSource[3][z][3]=val;
+
+  dataSource[0][z][3]=val;
+  dataSource[1][z][2]=val;
+  dataSource[2][z][1]=val;
+  dataSource[3][z][0]=val;
 }
 void testPattern() {
 
-    dataSource[0][0][0]=1;
-    dataSource[1][0][1]=1;
-    dataSource[2][0][2]=1;
-    dataSource[3][0][3]=1;
-    
-    dataSource[0][0][3]=1;
-    dataSource[1][0][2]=1;
-    dataSource[2][0][1]=1;
-    dataSource[3][0][0]=1;
+  stateChange(100);
+
+  if (counter==0){
+    writeValueToEntireDataSource(0);
+    Xpat(0,1);
+  }
+  if (counter==1){
+    writeValueToEntireDataSource(0);
+    Xpat(1,1);
+  }
+  if (counter==2){
+    writeValueToEntireDataSource(0);
+    Xpat(2,1);
+  }
+  if (counter==3){
+    writeValueToEntireDataSource(0);
+    Xpat(3,1);
+  }
+  if (counter==4){
+    writeValueToEntireDataSource(0);
+    Xpat(3,1);
+    counter = 0;
+  }
+}
+
+void lineSweep(){
+  stateChange(100);
+  writeValueToEntireDataSource(0);
+  if (counter == 0){
+    makeSquareOnDataSourceLayer(0, CUBE_SIZE, 0, 1, 0, 1);
+  }
+  if (counter == 1){
+    makeSquareOnDataSourceLayer(0, CUBE_SIZE, 1, 2, 0, 1);
+  }
+  if (counter == 2){
+    makeSquareOnDataSourceLayer(0, CUBE_SIZE, 2, 3, 0, 1);
+  }
+  if (counter == 3){
+    makeSquareOnDataSourceLayer(0, CUBE_SIZE, 3, 4, 0, 1);
+  }
+  if (counter == 4){
+    makeSquareOnDataSourceLayer(0, CUBE_SIZE, 3, 4, 1, 1);
+  }
+  if (counter == 5){
+    makeSquareOnDataSourceLayer(0, CUBE_SIZE, 2, 3, 1, 1);
+  }
+  if (counter == 6){
+    makeSquareOnDataSourceLayer(0, CUBE_SIZE, 1, 2, 1, 1);
+  }
+  if (counter == 7){
+    makeSquareOnDataSourceLayer(0, CUBE_SIZE, 0, 1, 1, 1);
+  }
+  if (counter == 8){
+    makeSquareOnDataSourceLayer(0, CUBE_SIZE, 0, 1, 2, 1);
+  }
+  if (counter == 9){
+    makeSquareOnDataSourceLayer(0, CUBE_SIZE, 1, 2, 2, 1);
+  }
+  if (counter == 10){
+    makeSquareOnDataSourceLayer(0, CUBE_SIZE, 2, 3, 2, 1);
+  }
+  if (counter == 11){
+    makeSquareOnDataSourceLayer(0, CUBE_SIZE, 3, 4, 2, 1);
+  }
+  if (counter == 12){
+    makeSquareOnDataSourceLayer(0, CUBE_SIZE, 3, 4, 3, 1);
+  }
+  if (counter == 13){
+    makeSquareOnDataSourceLayer(0, CUBE_SIZE, 2, 3, 3, 1);
+  }
+  if (counter == 14){
+    makeSquareOnDataSourceLayer(0, CUBE_SIZE, 1, 2, 3, 1);
+  }
+  if (counter == 15){
+    makeSquareOnDataSourceLayer(0, CUBE_SIZE, 0, 1, 3, 1);
+  }
+  if (counter == 16){
+    makeSquareOnDataSourceLayer(0, CUBE_SIZE, 0, 1, 3, 1);
+    counter = 0;
+  }
+}
+
+void lightCubeFace(){
+  stateChange(300);
+  writeValueToEntireDataSource(0);
+  if (counter == 0){
+    makeCubeOnDataSourceLayer(0, CUBE_SIZE, 0, CUBE_SIZE, 0, 1, 1);
+  }
+  if (counter == 1){
+    makeCubeOnDataSourceLayer(0, CUBE_SIZE, 0, 1, 0, CUBE_SIZE, 1);
+  }
+  if (counter == 2){
+    makeCubeOnDataSourceLayer(0, CUBE_SIZE, 0, CUBE_SIZE, 3, 4, 1);
+  }
+  if (counter == 3){
+    makeCubeOnDataSourceLayer(0, 4, 3, 4, 0, 4, 1);
+  } 
+  if (counter == 4){
+    makeCubeOnDataSourceLayer(0, 4, 3, 4, 0, 4, 1);
+    counter = 0;
+  }
 }
 
 void raiseAllGrounds(){
@@ -259,13 +587,13 @@ void writeLayerToCube(int layer) {
   //Putting the clock low makes sure that no data is accidentally written to the register.
   digitalWrite(latchPin, 0);
   digitalWrite(clockPin, 0);
-  
+
   //Write a face of data to the cube
   shiftOut(dataPin, clockPin, MSBFIRST, data2);
   shiftOut(dataPin, clockPin, MSBFIRST, data1);
   //Close the latch so it can update
   digitalWrite(latchPin, 1);
-  
+
   //Next we need to select the correct layer by picking the right ground
   //Match the layer value with the correct pin stored in the layerAddress array
   if (data1!=0 || data2!=0){
@@ -290,7 +618,8 @@ void updateCubeWithDataSource() {
           //The check to j is to decicide which byte to write to
           if (j<2){
             data1 = data1 + ledAddress[i][j];
-          }else{
+          }
+          else{
             data2 = data2 + ledAddress[i][j];
           }
         }
@@ -303,7 +632,7 @@ void updateCubeWithDataSource() {
 
 ///Helper functions
 void makeData() {
-  
+
   //First Byte
   ledAddress[0][0] = 1;
   ledAddress[1][0] = 4; //Wiring mistake
@@ -313,7 +642,7 @@ void makeData() {
   ledAddress[1][1] = 64;
   ledAddress[2][1] = 32;
   ledAddress[3][1] = 16;
-  
+
   //Second Byte
   ledAddress[0][2] = 8;
   ledAddress[1][2] = 4;
@@ -323,24 +652,31 @@ void makeData() {
   ledAddress[1][3] = 64;
   ledAddress[2][3] = 32;
   ledAddress[3][3] = 16;
-  
+
   //Set up the ground array to correct pins only hard coding needed
-  layerAddress[0] = 2;
-  layerAddress[1] = 3;
-  layerAddress[2] = 4;
-  layerAddress[3] = 5;
+  layerAddress[0] = 3;
+  layerAddress[1] = 4;
+  layerAddress[2] = 5;
+  layerAddress[3] = 6;
 
 }
 
 ISR(TIMER2_OVF_vect)    
 {
-  millis_timer ++;
-  
+  millis_timer ++;  
 }
 unsigned int Millis()
 {
   int m = millis_timer;
   return m;
 }
+
+void event_Handler()
+{
+  count++;
+  counter=0;
+}
+
+
 
 
